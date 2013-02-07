560987680c86b2a97bb512ed602fcbf855cc42c6
bingomanatee <dave@wonderlandlabs.com>
2013-02-07 13:39:38 -0800

    inverted x, y scans

--- tests/neighbors_test.js
+++ tests/neighbors_test.js
@@ -27,20 +27,21 @@ if (true) {
 				},
 				function (err, city_topo) {
 					city_topo.compress_to_greyscale();
-					var neighbors = city_topo.neighbors(2, 2, 1)
+					var neighbors = city_topo.neighbors(2, 2, 1);
+					if (_DEBUG) console.log('neighbors: %s', util.inspect(neighbors));
 					var values = _.pluck(neighbors, 'value');
 					t.deepEqual(values, [
 						146, 146, 146,
 						146, 146, 146,
 						146, 146, 255], 'values of 1 pixel scan');
-					t.deepEqual(_.pluck(neighbors, 'x'), [
+					t.deepEqual(_.pluck(neighbors, 'y'), [
 						1, 1, 1,
 						2, 2, 2,
-						3, 3, 3]), 'x of 1 pixel scan';
-					t.deepEqual(_.pluck(neighbors, 'y'), [
+						3, 3, 3]), '1pns x of 1 pixel scan';
+					t.deepEqual(_.pluck(neighbors, 'x'), [
 						1, 2, 3,
 						1, 2, 3,
-						1, 2, 3], 'y of 1 pixel scan');
+						1, 2, 3], '1pns y of 1 pixel scan');
 					t.end();
 				});
 		});
@@ -61,15 +62,17 @@ if (true) {
 				t.deepEqual(values, [
 					146, 146, 146,
 					146, 146,
-					146, 146, 255], 'values of 1 pixel ring');
+					146, 146, 255], 'ipnsr values of 1 pixel ring');
 				t.deepEqual(_.pluck(neighbors, 'x'), [
-					1, 1, 1,
-					2, 2,
-					3, 3, 3], 'x of 1 pixel scan');
-				t.deepEqual(_.pluck(neighbors, 'y'), [
+
 					1, 2, 3,
-					1, 3,
-					1, 2, 3], 'y of 1 pixel scan');
+					1,    3,
+					1, 2, 3], 'ipnsr x of 1 pixel scan');
+				t.deepEqual(_.pluck(neighbors, 'y'), [
+					1, 1, 1,
+					2,    2,
+					3, 3, 3
+				], 'y of 1 pixel scan');
 				t.end();
 			});
 	}) // end tap.test 1 pixel neighbor scan
@@ -89,15 +92,15 @@ if (true) {
 				t.deepEqual(values, [
 					146, 146,
 					146, 146
-				], 'values of 1 pixel scan at ur');
+				], 'pnsaur values of 1 pixel scan at ur');
 				t.deepEqual(_.pluck(neighbors, 'x'), [
-					7, 7,
-					8, 8
-				], 'x of 1 pixel scan at ur');
-				t.deepEqual(_.pluck(neighbors, 'y'), [
 					7, 8,
 					7, 8
-				], 'y of 1 pixel scan at ur');
+				], 'pnsaur x of 1 pixel scan at ur');
+				t.deepEqual(_.pluck(neighbors, 'y'), [
+					7, 7,
+					8, 8
+				], 'pnsaur y of 1 pixel scan at ur');
 				t.end();
 
 			}) // end tap.test 1 pixel neighbor scan
@@ -123,22 +126,24 @@ if (true) {
 					t.deepEqual(values, [
 						146, 146, 146,
 						146, 146,
-						146, 146, 255], 'values of 1 pixel ring');
+						146, 146, 255], 'nsr values of 1 pixel ring');
 					t.deepEqual(_.pluck(neighbors, 'x'), [
-						1, 1, 1,
-						2, 2,
-						3, 3, 3], 'x of 1 pixel scan');
-					t.deepEqual(_.pluck(neighbors, 'y'), [
 						1, 2, 3,
-						1, 3,
-						1, 2, 3], 'y of 1 pixel scan');
+						1,    3,
+						1, 2, 3
+					], 'nsr x of 1 pixel scan');
+					t.deepEqual(_.pluck(neighbors, 'y'), [
+						1, 1, 1,
+						2,    2,
+						3, 3, 3
+					], 'nsr y of 1 pixel scan');
 					t.end();
 				});
 		});
 	}) // end tap.test 1 pixel neighbor scan
 }
 
-if (true) {
+if (false) {
 	tap.test('test 2 pixel neighbor scan', function (t) {
 		fs.readFile(cityhouse_file, function (err, cityhouse) {
 			if (err) throw err;
@@ -160,18 +165,18 @@ if (true) {
 						146, 146, 146, 146, 146,
 						146, 146, 146, 255, 255,
 						146, 146, 146, 255, 255], '2 pixel scan values');
-					t.deepEqual(_.pluck(neighbors, 'x'), [
+					t.deepEqual(_.pluck(neighbors, 'y'), [
 						0, 0, 0, 0, 0,
 						1, 1, 1, 1, 1,
 						2, 2, 2, 2, 2,
 						3, 3, 3, 3, 3,
-						4, 4, 4, 4, 4], '2 pixel scan x');
-					t.deepEqual(_.pluck(neighbors, 'y'), [
+						4, 4, 4, 4, 4], '2 pixel scan y');
+					t.deepEqual(_.pluck(neighbors, 'x'), [
 						0, 1, 2, 3, 4,
 						0, 1, 2, 3, 4,
 						0, 1, 2, 3, 4,
 						0, 1, 2, 3, 4,
-						0, 1, 2, 3, 4], '2 pixel scan y');
+						0, 1, 2, 3, 4], '2 pixel scan x');
 					t.end();
 				});
 		});
@@ -201,18 +206,18 @@ if (true){
 							146, 146,
 							146, 255,
 							146, 146, 146, 255, 255], '2 pixel ring balues');
-					t.deepEqual(_.pluck(neighbors, 'x'), [
+					t.deepEqual(_.pluck(neighbors, 'y'), [
 						0, 0, 0, 0, 0,
 						1, 1,
 						2, 2,
 						3, 3,
-						4, 4, 4, 4, 4], '2 pixel san x');
-					t.deepEqual(_.pluck(neighbors, 'y'), [
+						4, 4, 4, 4, 4], '2 pixel san y');
+					t.deepEqual(_.pluck(neighbors, 'x'), [
 						0, 1, 2, 3, 4,
 						0, 4,
 						0, 4,
 						0, 4,
-						0, 1, 2, 3, 4], '2 pixel scan y');
+						0, 1, 2, 3, 4], '2 pixel scan x');
 					t.end();
 				});
 		});
