94cc510ae77659ca2c99d0caea4d687f7c8bba7b
pc-dedelhart <david.edelhart@personalcapital.com>
2013-02-07 10:19:45 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node-Topography-Canvas
+
+In order to make node-topography browserify-ale the canvas-centric features
+of node-topography are being moved here.
---
+++ index.js
@@ -0,0 +1,37 @@
+var _mixins = require('./lib/mixins');
+var _ = require('underscore');
+var topo = require('node-topography');
+var Gate = require('gate');
+
+function TopoGrid(mixins, config, cb) {
+	if (!mixins) {
+		mixins = {};
+	}
+
+	_.extend(mixins, _mixins);
+
+	return topo.TopoGrid(mixins, config, cb);
+}
+
+module.exports = {
+	TopoGrid: TopoGrid,
+
+	image_file_topos: function (image_files, cb) {
+		var gate = Gate.create();
+
+		var TopoGrid = module.exports.TopoGrid;
+
+		_.each(image_files, function (file) {
+			TopoGrid({}, {source: file, source_type: 'image_file'}, gate.latch());
+		});
+
+		gate.await(function (err, topo_grids) {
+			cb(null, _.reduce(_.values(topo_grids),
+				function (out, props) {
+					if (props[0]) throw props[0];
+					out.push(props[1]);
+					return out;
+				}, []));
+		});
+	}
+};
